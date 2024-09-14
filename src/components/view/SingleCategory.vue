9c083ddd1c3de32c3fe2edf9623c6a3a836e79ad
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-14 19:01:48 +0600

    feat: fetch meals by category

--- src/components/view/SingleCategory.vue
+++ src/components/view/SingleCategory.vue
@@ -1,9 +1,33 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
-const categoryName = ref(route.params.category);
+const categoryName = route.params.category;
+
+let meals = ref([]);
+let isLoading = ref(false);
+
+onMounted(async () => {
+  try {
+    isLoading.value = true;
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+    );
+
+    if (!res.ok) {
+      throw new Error(`API request failed with status ${res.status}`);
+    }
+
+    const data = await res.json();
+    const result = data?.meals;
+    meals.value = result;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
 <template>
