c4f6da0fc070882c83fd26473d502c4307c3ad57
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-15 14:01:36 +0600

    feat: update about page

--- src/components/view/AboutView.vue
+++ src/components/view/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="w-full max-w-screen-lg mx-auto px-4 py-16">
-    <h1 class="text-3xl text-rose-500">This is aBOUT Page</h1>
+    <h1 class="text-3xl text-center font-medium tracking-tighter">
+      This is About Page
+    </h1>
   </section>
 </template>
